5b8241a52fdbbed08ea7aa0b46f5e565822da7a1
Mikey-Phoenix <bajomichael06@gmail.com>
2026-09-19 20:07:14 +0100

    fixed search block sizes

--- src/components/morenewsblock.tsx
+++ src/components/morenewsblock.tsx
@@ -9,7 +9,7 @@ export default function MoreNewsBlock({title, description, imageUrl, story, date
         return (
             <>
                 <div className="w-full min-h-[200px] p-4 mb-2 flex flex-col-reverse md:flex-row  justify-between rounded-md bg-gray-100 cursor-pointer hover:shadow-md" onClick={()=>{redirect(story)}}>
-                    <div className="flex flex-col h-[160px] justify-evenly ">
+                    <div className="flex flex-col md:h-[160px] justify-evenly ">
                         <h2 className="text-xl md:text-2xl font-bold text-[var(--tertiary)]">{title}</h2>
                         <p className="text-sm">{description}</p>
                         <p className="text-[var(--secondary)] italic text-sm">{location}, {date}  {time}</p>
